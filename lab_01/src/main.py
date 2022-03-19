e8c5b1b75037d7bc29d516ed6ded28a82546a843
ilyasssklimov <ilyasssklimov@gmail.com>
2022-03-19 14:30:03 +0300

    done lab_01

--- lab_01/src/main.py
+++ lab_01/src/main.py
@@ -1,5 +1,14 @@
 import math as m
+from matplotlib import pyplot as plt
 from prettytable import PrettyTable
+
+
+def create_graphic(x, y, name):
+    plt.title(name)
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.plot(x, y)
+    plt.show()
 
 
 class CauchyProblem:
@@ -58,30 +67,52 @@ class CauchyProblem:
 
         return y_out
 
-    def solve_equation(self):
+    def solve_equation(self, print_table=True, show_graphics=False):
         table = PrettyTable()
-        table.field_names = ['x', 'Пикар (1)', 'Пикар (2)', 'Пикар (3)', 'Пикар (4)', 'Эйлер', 'Рунге-Кутта']
+        method_names = ['Пикар (1)', 'Пикар (2)', 'Пикар (3)', 'Пикар (4)', 'Эйлер', 'Рунге-Кутта']
+        table.field_names = ['x', *method_names]
 
         h = 1e-5
         x, y0, end = 0, 0, 2.0
         n = m.ceil((end - x) / h) + 1
 
         x_arr = [x + h * i for i in range(n)]
-        y1 = self.runge_kutta(n, h, x, y0)
+        y1 = self.picard(n, h, x, y0)
         y2 = self.euler(n, h, x, y0)
-        y3 = self.picard(n, h, x, y0)
+        y3 = self.runge_kutta(n, h, x, y0)
 
         step = int(n / 100)
         for i in range(0, n, step):
-            table.add_row([round(x_arr[i], 8), round(y3[0][i], 8), round(y3[1][i], 8),
-                           round(y3[2][i], 8), round(y3[3][i], 8), round(y1[i], 8), round(y2[i], 8)])
+            table.add_row([round(x_arr[i], 8), round(y1[0][i], 8), round(y1[1][i], 8),
+                           round(y1[2][i], 8), round(y1[3][i], 8), round(y2[i], 8), round(y3[i], 8)])
 
-        print(table)
+        if print_table:
+            print(table)
+
+        if show_graphics:
+            x_arr_neg = [x - h * i for i in range(n)]
+            x_arr_neg.reverse()
+            x_arr = x_arr_neg + x_arr
+            y_arr = []
+
+            y1_neg = self.picard(n, -h, x, y0)
+            y_arr.extend([list(reversed(y1_neg_i)) + y1_i for y1_neg_i, y1_i in zip(y1_neg, y1)])
+
+            y2_neg = self.euler(n, -h, x, y0)
+            y2_neg.reverse()
+            y_arr.append(y2_neg + y2)
+
+            y3_neg = self.runge_kutta(n, -h, x, y0)
+            y3_neg.reverse()
+            y_arr.append(y3_neg + y3)
+
+            for name, y in zip(method_names, y_arr):
+                create_graphic(x_arr, y, name)
 
 
 def main():
     cauchy = CauchyProblem(lambda x, u: x ** 2 + u ** 2)
-    cauchy.solve_equation()
+    cauchy.solve_equation(print_table=True, show_graphics=False)
 
 
 if __name__ == '__main__':
